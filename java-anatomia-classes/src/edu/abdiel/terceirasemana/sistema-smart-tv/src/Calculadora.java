0c29d37878cb25f48ee5f0f056ef316f6c51cc6c
Abdiel Athayde <abdieldeathayde67@gmail.com>
2023-01-04 21:03:30 -0300

    feat atualizando projeto bootcamp basico

---
+++ java-anatomia-classes/src/edu/abdiel/terceirasemana/sistema-smart-tv/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <br>Note:<br> Leia atentamente a documentação destas classes para desfrutar dos recursos oferecfidos pelo autor
+ * 
+ * @author Abdiel de Athayde
+ * @version 1.0
+ * @since 04/01/2023
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois numeros inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ java-anatomia-classes/src/edu/abdiel/terceirasemana/sistema-smart-tv/src/Comentarios.java
@@ -0,0 +1,47 @@
+public class Comentarios {
+    main 
+
+    public static void main(String[] args) {
+        //Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+        
+
+         /** 
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+
+
+//         public void method(){
+//         }
+
+/*
+ * Este método foi elaborado as pressas
+ * por isso eu abreviei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou multiplicar
+ * dois numeros
+ */
+
+public int somaMultiplica (int n, int x, String m){
+    int r = 0; // r é igual ao resultado
+    if (m == "M"){ //M = multiplicação
+        r = n * x;
+    } else {
+        //se não soma mesmo
+        r = n + x;
+    }
+    return r;
+}
+
+
+    }
+    
+}
