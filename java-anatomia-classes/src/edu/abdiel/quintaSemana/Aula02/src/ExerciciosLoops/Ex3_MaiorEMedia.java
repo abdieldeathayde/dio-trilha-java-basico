e89c8207a16fd31a0c3628d5a290c61dfe2eca40
Abdiel Athayde <abdieldeathayde67@gmail.com>
2023-01-11 17:49:55 -0300

    fechando scanner Ex3_MaiorEMedia java

--- java-anatomia-classes/src/edu/abdiel/quintaSemana/Aula02/src/ExerciciosLoops/Ex3_MaiorEMedia.java
+++ java-anatomia-classes/src/edu/abdiel/quintaSemana/Aula02/src/ExerciciosLoops/Ex3_MaiorEMedia.java
@@ -34,7 +34,7 @@ public class Ex3_MaiorEMedia {
         System.out.println("Maior: " + maior);
         System.out.println("Média: " + (soma/5));
 
-        scan.close()
+        scan.close();
     }
     
 }
