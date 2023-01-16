ecdb380cbd2065149eec9c723eea111375bdbcc8
Abdiel Athayde <abdieldeathayde67@gmail.com>
2023-01-16 10:46:11 -0300

    atualizando classe Emprestimo Java

---
+++ java-anatomia-classes/src/edu/abdiel/quintaSemana/Aula02/src/ExerciciosMetodos/Main.java
@@ -0,0 +1,11 @@
+package ExerciciosMetodos;
+
+public class Main {
+    public static void main(String[] args) {
+        //Empréstimo
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+    }
+}
--- java-anatomia-classes/src/edu/abdiel/quintaSemana/Aula02/src/ExerciciosMetodos/main.java
+++
@@ -1,5 +0,0 @@
-package ExerciciosMetodos;
-
-public class main {
-    
-}
